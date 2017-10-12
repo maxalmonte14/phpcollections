9dbfd15383cff39ff7b03f06647be42c168a31f4
Max Almonte <maxalmonte14@hotmail.com>
2017-10-12 00:49:29 -0400

    Created for manage global utilities

---
+++ src/utils.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Return the correct article
+ * for preceding a specific word
+ *
+ * @param  string $word
+ * @return string
+ */
+function getArticle(string $word) :string
+{
+    return !in_array(strtolower($word[0]), ['a','e','i','o','u']) ? 'a' : 'an';
+}
