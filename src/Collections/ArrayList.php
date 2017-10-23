c76c369474faa6cbf5ccdf17761a61abfd7fa956
Max Almonte <maxalmonte14@hotmail.com>
2017-10-23 11:46:39 -0400

    Added ArrayList class

---
+++ src/Collections/ArrayList.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace PHPCollections\Collections;
+
+use PHPCollections\Exceptions\InvalidOperationException;
+
+class ArrayList extends BaseCollection 
+{
+    /**
+     * Add a new element to the collection
+     *
+     * @param  mixed $val
+     * @return void
+     */
+    public function add($val)
+    {
+        $this->data[] = $val;
+    }
+
+    /**
+     * Check if the collection
+     * contains a given value
+     *
+     * @param  mixed $val
+     * @return bool
+     */
+    public function contains($val)
+    {
+        foreach ($this->data as $value)
+            if ($value === $val) return true;
+
+        return false;
+    }
+
+    /**
+     * Return all the coincidences found
+     * for the given callback or null
+     * 
+     * @param  callable $callback
+     * @return PHPCollections\ArrayList|null
+     */
+    public function filter(callable $callback)
+    {
+        $matcheds = [];
+        foreach ($this->data as $value) {
+            if (call_user_func($callback, $value) === true)
+                $matcheds[] = $value;
+        }
+        return count($matcheds) > 0 ? new $this(array_values($matcheds)) : null;
+    }
+
+        /**
+     * Search one or more elements in
+     * the collection
+     * 
+     * @param  callable $callback
+     * @param  boolean  $shouldStop
+     * @return PHPCollections\ArrayList|null
+     */
+    public function find(callable $callback, $shouldStop = false)
+    {
+        $matcheds = [];
+        foreach ($this->data as $key => $item) {
+            if ($callback($item, $key) === true) {
+                $matcheds[] = $item;
+                if ($shouldStop) break;
+            }
+        }
+        return count($matcheds) > 0 ? new $this($matcheds) : null;
+    }
+
+    /**
+     * Get the first element of the collection
+     *
+     * @throws OutOfRangeException
+     * @return mixed
+     */
+    public function first()
+    {
+        if ($this->count() == 0)
+            throw new OutOfRangeException("You're trying to get data into an empty collection.");
+        return $this->data[0];
+    }
+
+    /**
+     * Get the element specified 
+     * at the given index
+     *
+     * @param  int   $offset
+     * @return mixed
+     */
+    public function get($offset)
+    {
+        return $this->offsetGet($offset);
+    }
+
+    /**
+     * Get the last element of the collection
+     *
+     * @throws OutOfRangeException
+     * @return mixed
+     */
+    public function last()
+    {
+        if ($this->count() == 0)
+            throw new OutOfRangeException("You're trying to get data into an empty collection.");
+        return $this->data[$this->count() - 1];
+    }
+
+    /**
+     * Update elements in the collection by
+     * applying a given callback function
+     *
+     * @param  callable $callback
+     * @return PHPCollections\ArrayList|null
+     */
+    public function map(callable $callback)
+    {
+        $matcheds = array_map($callback, $this->data);
+        return count($matcheds) > 0 ? new $this(array_values($matcheds)) : null;
+    }
+
+    /**
+     * Merge new data with the actual
+     * collection and returns a new one
+     *
+     * @param  array $data
+     * @return PHPCollections\ArrayList
+     */
+    public function merge(array $data)
+    {
+        return new $this(array_merge($this->data, $data));
+    }
+    
+    /**
+     * Return a random element of 
+     * the collection
+     *
+     * @throws PHPCollections\Exceptions\InvalidOperationException
+     * @return mixed
+     */
+    public function rand()
+    {
+        if ($this->count() == 0)
+            throw new InvalidOperationException('You cannot get a random element from an empty collection.');
+        $randomIndex = array_rand($this->data);
+        return $this->get($randomIndex);
+    }
+
+    /**
+     * Return a new collection with the
+     * reversed values
+     * 
+     * @throws PHPCollections\Exceptions\InvalidOperationException
+     * @return PHPCollections\ArrayList
+     */
+    public function reverse()
+    {
+        if ($this->count() == 0)
+            throw new InvalidOperationException('You cannot reverse an empty collection.');
+        return new $this(array_reverse($this->data));
+    }
+
+    /**
+     * Update the value of the element 
+     * at the given index
+     *
+     * @param  int   $index
+     * @param  mixed $value
+     * @return bool
+     */
+    public function update($index, $value)
+    {
+        if (!$this->exists($index))
+            throw new InvalidOperationException('You cannot update a non-existent value');
+        $this->data[$index] = $value;
+        return $this->data[$index] === $value;
+    }
+}
---
+++ tests/Unit/ArrayListTest.php
@@ -0,0 +1,155 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use PHPCollections\Collections\ArrayList;
+
+class ArrayListTest extends TestCase
+{
+    private $arrayList;
+
+    public function setUp()
+    {
+        $this->arrayList = new ArrayList();
+        $this->arrayList->add('Max');
+        $this->arrayList->add(5);
+        $this->arrayList->add(false);
+        $this->arrayList->add(new StdClass());
+        $this->arrayList->add(null);
+    }
+
+    /** @test */
+    public function canAddElements()
+    {
+        $this->arrayList->add('last index');
+        $lastIndex = $this->arrayList->count() - 1;
+        $this->assertEquals('last index', $this->arrayList->get($lastIndex));
+    }
+
+    /** @test */
+    public function canClearData()
+    {
+        $this->arrayList->clear();
+        $this->assertEmpty($this->arrayList);
+    }
+
+    /** @test */
+    public function canCheckIfIndexExists()
+    {
+        $this->assertTrue($this->arrayList->exists(0));
+    }
+
+    /** @test */
+    public function canFilterByValue()
+    {
+        $newArrayList = $this->arrayList->filter(function ($value) {
+            return is_string($value);
+        });
+
+        $this->assertNotNull($newArrayList);
+        $this->assertCount(1, $newArrayList);
+        $this->assertEquals('Max', $newArrayList->get(0));
+    }
+
+    /** @test */
+    public function canCheckIfAValueIsContained()
+    {
+        $isContained = $this->arrayList->contains(null);
+        $this->assertTrue($isContained);
+    }
+
+    /** @test */
+    public function canGetFirstElement()
+    {
+        $firstElement = $this->arrayList->first();
+        $this->assertEquals('Max', $firstElement);
+    }
+
+    /** @test */
+    public function canGetLastElement()
+    {
+        $lastElement = $this->arrayList->last();
+        $this->assertNull($lastElement);
+    }
+
+    /** @test */
+    public function canUpdateTheElementsByMapping()
+    {
+        $newArrayList = $this->arrayList->map(function ($val) {
+            if ($val == false) return "Empty value";
+            else return $val;
+        });
+        $this->assertContains("Empty value", $newArrayList->toArray());
+    }
+
+    /** @test */
+    public function canFindMatchingElements()
+    {
+        $elements = $this->arrayList->find(function ($v, $k) {
+            return $v != false;
+        });
+        $this->assertCount(3, $elements);
+        $this->assertFalse($elements->contains(false));
+        $this->assertFalse($elements->contains(null));
+
+        $singleElement = $this->arrayList->find(function ($v, $k) {
+            return !is_string($v);
+        }, true);
+        $this->assertCount(1, $singleElement);
+        $this->assertEquals(5, $singleElement->get(0));
+    }
+
+    /** @test */
+    public function canMergeNewDataIntoNewArrayList()
+    {
+        $numbers = new ArrayList([1,2,3,4,5]);
+        $newArrayList = $numbers->merge([6,7,8,9,10]);
+        $this->assertCount(10, $newArrayList);
+        $this->assertEquals(1, $newArrayList->first());
+        $this->assertEquals(10, $newArrayList->last());
+    }
+
+    /** 
+     * @test 
+     * @expectedException PHPCollections\Exceptions\InvalidOperationException
+     */
+    public function cannotGetARandomValueFromEmptyArrayList()
+    {
+        $newArrayList = new ArrayList();
+        $newArrayList->rand(); // Here an InvalidOperationException is thrown!
+    }
+
+    /** @test */
+    public function canReverse()
+    {
+        $reversedList = $this->arrayList->reverse();
+        $this->assertEquals(null, $reversedList->first());
+        $this->assertEquals('Max', $reversedList->last());
+    }
+
+    /** 
+     * @test
+     * @expectedException PHPCollections\Exceptions\InvalidOperationException
+     */
+    public function cannotReverseAnEmptyArrayList()
+    {
+        $newArrayList = new ArrayList();
+        $reversedList = $newArrayList->reverse(); // Here an InvalidOperationException is thrown!
+    }
+
+    /** @test */
+    public function canUpdateAValue()
+    {
+        $this->arrayList->update(0, 'John');
+        $this->assertEquals('John', $this->arrayList->get(0));
+    }
+
+    /** 
+     * @test
+     * @expectedException PHPCollections\Exceptions\InvalidOperationException
+     */
+    public function cannotUpdateAnNonExistingValue()
+    {
+        $newArrayList = new ArrayList();
+        $newArrayList->update(0, 'Some value'); // Here an InvalidOperationException is thrown!
+    }
+}
