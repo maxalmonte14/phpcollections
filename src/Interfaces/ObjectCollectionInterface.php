adc7bac61f1344f9e2f257a92e37f971ef818c33
Max Almonte <maxalmonte14@hotmail.com>
2018-10-07 13:36:48 -0400

    Added interface for object collections

---
+++ src/Interfaces/ObjectCollectionInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPCollections\Interfaces;
+
+interface ObjectCollectionInterface
+{
+    public function add(object $value): void;
+    
+    public function get(int $offset);
+
+    public function remove(int $offset): void;
+
+    public function update(int $offset, object $value): bool;
+}
